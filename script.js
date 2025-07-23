7258111911f82a10a8d66d4db89039a7d3bb4ca4
Gabriel Victor <gabrielvictorribeiro84@gmail.com>
2025-07-23 10:59:26 -0300

    Conversor de moedas

---
+++ script.js
@@ -0,0 +1,49 @@
+const botao_conversao = document.getElementById("botao-conversao")
+const select_moeda_estrangeira = document.getElementById("select-moeda-estrangeira")
+
+const dolar = 5
+const euro = 6
+
+function converter_valores() {
+  const input_real = document.getElementById("input-real").value
+  const valor_real_texto = document.getElementById("valor-real-texto")
+  const valor_estrangeiro_texto = document.getElementById("valor-estrangeiro-texto")
+  
+  valor_real_texto.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(input_real)
+  
+  if (select_moeda_estrangeira.value === "US$ Dólar estadunidense") {
+    valor_estrangeiro_texto.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(input_real / dolar)
+  }
+  
+  if (select_moeda_estrangeira.value === "€ Euro") {
+    valor_estrangeiro_texto.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(input_real / euro)
+  }
+  
+}
+
+function mudar_moeda() {
+  const moeda_nome = document.getElementById("moeda-nome")
+  const bandeira_estrangeira = document.getElementById("bandeira-estrangeira")
+  
+  if (select_moeda_estrangeira.value === "€ Euro") {
+    moeda_nome.innerHTML = "Euro"
+    bandeira_estrangeira.src = "imagens/europa.png"
+    
+  } else if (select_moeda_estrangeira.value === "US$ Dólar estadunidense") {
+    moeda_nome.innerHTML = "Dólar estadunidense"
+    bandeira_estrangeira.src = "imagens/usa.png"
+  }
+  converter_valores()
+}
+
+select_moeda_estrangeira.addEventListener("change", mudar_moeda)
+botao_conversao.addEventListener("click", converter_valores)
